0a10a1a25a452a0d0a12f6b51587802da7f062da
Chaima BEKHAOUDA <137110936+chaima-bekhaouda@users.noreply.github.com>
2023-11-22 10:06:07 +0100

    Add files via upload

---
+++ Jour1/Job9/main.py
@@ -0,0 +1,8 @@
+# Chaîne à vérifier
+chaine = "Hello World"
+
+# Vérifier si la chaîne contient le caractère 'e'
+if 'e' in chaine:
+    print("La chaîne contient le caractère 'e'.")
+else:
+    print("La chaîne ne contient pas le caractère 'e'.")
